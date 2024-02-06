dc6f28cad89fa5b1b8869cecb60a7470ea41225d
Chandra.kyc <chandra.ck101@gmail.com>
2024-02-06 13:02:44 -0600

    Eslint fix

--- src/containers/certifications/Certifications.js
+++ src/containers/certifications/Certifications.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import "./Certifications.css";
-import { Fade } from "react-reveal";
-import { certifications } from "../../portfolio";
-import CertificationCard from "../../components/certificationCard/CertificationCard";
+// import { Fade } from "react-reveal";
+// import { certifications } from "../../portfolio";
+// import CertificationCard from "../../components/certificationCard/CertificationCard";
 
 class Certifications extends Component {
   render() {
-    const theme = this.props.theme;
+    // const theme = this.props.theme;
     return (
       <div className="main" id="certs">
         {/* <div className="certs-header-div">
